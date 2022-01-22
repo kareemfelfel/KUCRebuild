25cb27895455f330caa444820951c5a9984e3120
kareemfelfel <kareemfelfel@yahoo.com>
2022-01-22 16:53:24 -0500

    Added map testing for 3 different map types including google maps.

--- Controller/controller.php
+++ Controller/controller.php
@@ -5,5 +5,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo '<h1> This is the controller </h1>';
 
+
+include '../View/includes/head.php';
+include '../View/includes/navbar.php';
+include '../View/plotTest.php';
