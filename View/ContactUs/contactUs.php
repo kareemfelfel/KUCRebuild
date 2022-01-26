5a51ed483a9aa43cf05c1d1ab07412147c997917
kareemfelfel <kareemfelfel@yahoo.com>
2022-01-26 12:32:11 -0500

    Kareem: updated naming files again.

--- View/ContactUs/contactUs.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* 
- * This page has public access
- * 
- * the Contact Us page should not need to contact the controler at all.
- * Only Front-End hard-coded information will be provided here.
- */
-
