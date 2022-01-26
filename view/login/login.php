507e7d151e86d0b7de0cd20576a6836e85241b98
kareemfelfel <kareemfelfel@yahoo.com>
2022-01-26 12:35:29 -0500

    Kareem: changed login directory name.

---
+++ view/login/login.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 
+ * This page has public access
+ * 
+ * Simple log-in form with AJAX to confirm the user identity.
+ * Once Identity is confirmed, direct the user to the search page.
+ */
+
