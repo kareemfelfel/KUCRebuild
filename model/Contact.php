0c067a43214ede6b6d645c21b4b03508f428c28c
kareemfelfel <kareemfelfel@yahoo.com>
2022-02-17 22:23:24 -0500

    KUCR-39: added contact table data class.

---
+++ model/Contact.php
@@ -0,0 +1,20 @@
+<?php
+
+Class Contact{
+    public $id;
+    public $firstName;
+    public $lastName;
+    public $email;
+    public $title;
+    public $phoneNumber;
+    
+    function __construct($row) {
+        $this -> id = $row['ID'];
+        $this -> firstName = $row['FIRST_NAME'];
+        $this -> lastName = $row['LAST_NAME'];
+        $this -> email = $row['EMAIL'];
+        $this -> title = $row['TITLE'];
+        $this -> phoneNumber = $row['PHONE_NUMBER'];
+    }
+}
+
