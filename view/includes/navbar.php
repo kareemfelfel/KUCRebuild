3cc6c4ffd3a0a74b0b6221a28f1f9ebfceb38ca7
kareemfelfel <kareemfelfel@yahoo.com>
2022-03-11 09:35:13 +0000

    changed navbar color

--- view/includes/navbar.php
+++ view/includes/navbar.php
@@ -1,5 +1,5 @@
 
-<nav class="navbar navbar-expand-lg navbar-toggleable-md navbar-dark" style="background-color: #800080">
+<nav class="navbar navbar-expand-lg navbar-toggleable-md navbar-dark" style="background-color: lightslategray">
     
     
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
