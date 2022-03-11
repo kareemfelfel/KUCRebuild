83f691ce0fb55e1eb4ecf41710a65866525ce075
kareemfelfel <kareemfelfel@yahoo.com>
2022-03-11 09:20:53 +0000

    Cleared attached files.

--- controller/controller.php
+++ controller/controller.php
@@ -435,7 +435,7 @@ function fetchTombById(){
             "location" =>$result->sectionLetter->letter . " " .$result->lotNumber,
             "hasOpenPlots" =>$result->hasOpenPlots,
             "forSale" => $result->forSale,
-            "purchaseDate" => date("m-d-Y", strtotime($result->purchaseDate)),
+            "purchaseDate" => isset($result->purchaseDate)? date("m-d-Y", strtotime($result->purchaseDate)) : "N/A",
             "price" => $result->price,
             "mainImage" => $result->mainImage,
             "attachments" => isset($result->attachments) ?
