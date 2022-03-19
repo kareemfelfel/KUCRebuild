b0be3b05932486ab7c4323fd335baa0c2c09077f
kareemfelfel <kareemfelfel@yahoo.com>
2022-03-19 06:01:32 +0100

    KUCR-167: Added addContact fetching point.

--- controller/controller.php
+++ controller/controller.php
@@ -189,6 +189,9 @@ switch ($action)
     case"addTombSectionLetter":
         addTombSectionLetter();
         break;
+    case"addContact":
+        addContact();
+        break;
 }
 //-----API--------
 function fetchAllOwnersList(){
@@ -974,6 +977,56 @@ function addTombSectionLetter(){
     if(empty($response->error)){
         $obj = new ToSectionLetter($sectionLetter);
         $modelResponse = insertTombSectionLetter($obj);
+        for($i = 0; $i< count($modelResponse->error); $i++){
+            $response->addError($modelResponse->error[$i]);
+        }
+
+        if(count($response->error) == 0 && count($modelResponse->result) == 1){ // If everything was successful, send client true in response
+            $response->addResult($modelResponse->result[0]);
+        }
+    }
+    echo json_encode(get_object_vars($response));
+}
+
+function addContact(){
+    $response = new Response();
+   
+    // Getting REQUEST data
+    $data = json_decode($_POST['request']);
+    $firstName = !empty($data->firstName) ? $data->firstName : null;
+    $lastName = !empty($data->lastName) ? $data->lastName : null;
+    $email = !empty($data->email) ? $data->email : null;
+    $title = !empty($data->title) ? $data->title : null;
+    $phoneNumber = !empty($data->phoneNumber) ? $data->phoneNumber : null;
+    
+    if(!isset($firstName) || strlen($firstName) < 1){
+        $response->addError("First Name must be of a valid value.");
+    }
+    if(!isset($lastName) || strlen($lastName) < 1){
+        $response->addError("Last Name must be of a valid value.");
+    }
+    if(isset($phoneNumber)){
+        if(!is_numeric($phoneNumber)){
+            $response->addError("Phone number must consist of numbers only.");
+        }
+        if(strlen($phoneNumber) != 10){
+            $response->addError("Only US phone numbers of 10 digits are accepted.");
+        }
+        // If phone number is valid, add +1 to the beginning of the string.
+        if(empty($response->error)){
+            $phoneNumber = "+1" . $phoneNumber;
+        }
+    }
+    else{
+        $response->addError("Phone Number must be set.");
+    }
+    if (isset($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $response->addError("Invalid email address.");
+    }
+    
+    if(empty($response->error)){
+        $obj = new ToContactTable($firstName, $lastName, $email, $title, $phoneNumber);
+        $modelResponse = insertContact($obj);
         for($i = 0; $i< count($modelResponse->error); $i++){
             $response->addError($modelResponse->error[$i]);
         }
