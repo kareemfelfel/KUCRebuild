0df72fae8d60c31bc5cac38f11f265d130b044de
kareemfelfel <kareemfelfel@yahoo.com>
2022-03-27 20:55:27 +0100

    Kareem: Added edit columbarium and edit lot cards to the administration page.

--- view/administration/administration.php
+++ view/administration/administration.php
@@ -61,12 +61,58 @@
         
         <div class="col-md-4 col-sm-4">
             <div class="wrimagecard wrimagecard-topimage">
+                <a href="controller.php?action=directToEditColumbariumSelectorPage">
+                    <div class="wrimagecard-topimage_header" style="background-color:rgba(204, 207, 117, 0.1) ">
+                      <center><i class="fa fa-solid fa-warehouse" style="color:#CCCF75"></i></center>
+                    </div>
+                    <div class="wrimagecard-topimage_title">
+                      <h4 class="text-center">Columbarium
+                      </h4>
+                        <p class="text-center" style="font-weight: 100; font-size: 15px;">Edit</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        
+        <div class="col-md-4 col-sm-4">
+            <div class="wrimagecard wrimagecard-topimage">
+                <a href="controller.php?action=directToEditTombSelectorPage">
+                    <div class="wrimagecard-topimage_header" style="background-color:rgba(180, 176, 185, 0.1) ">
+                      <center><i class="fa fa-solid fa-cross" style="color:#B4B0B9"></i></center>
+                    </div>
+                    <div class="wrimagecard-topimage_title">
+                      <h4 class="text-center">Lot
+                      </h4>
+                        <p class="text-center" style="font-weight: 100; font-size: 15px;">Edit</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        
+        <div class="col-md-4 col-sm-4">
+            <div class="wrimagecard wrimagecard-topimage">
                 <a href="controller.php?action=directToOwnersPage">
                     <div class="wrimagecard-topimage_header" style="background-color:rgba(128, 0, 128, 0.1) ">
                       <center><i class="fa fa-solid fa-id-card" style="color:#800080"></i></center>
                     </div>
                     <div class="wrimagecard-topimage_title">
                       <h4 class="text-center">Owners
+                      </h4>
+                        <p class="text-center" style="font-weight: 100; font-size: 15px;">Add & Edit</p>
+                    </div>
+                </a>
+            </div>
+        </div>     
+    </div> <!<!-- End of second row -->
+    <div class="row">
+        <div class="col-md-4 col-sm-4">
+            <div class="wrimagecard wrimagecard-topimage">
+                <a href="controller.php?action=directToContactsPage">
+                    <div class="wrimagecard-topimage_header" style="background-color:rgba(3, 173, 252, 0.1) ">
+                      <center><i class=" fa fa-solid fa-user-group" style="color:#03adfc"></i></center>
+                    </div>
+                    <div class="wrimagecard-topimage_title">
+                      <h4 class="text-center">Contacts
                       </h4>
                         <p class="text-center" style="font-weight: 100; font-size: 15px;">Add & Edit</p>
                     </div>
@@ -99,22 +145,6 @@
                       <h4 class="text-center">New Admins
                       </h4>
                         <p class="text-center" style="font-weight: 100; font-size: 15px;">Add</p>
-                    </div>
-                </a>
-            </div>
-        </div>
-    </div> <!<!-- End of second row -->
-    <div class="row">
-        <div class="col-md-4 col-sm-4">
-            <div class="wrimagecard wrimagecard-topimage">
-                <a href="controller.php?action=directToContactsPage">
-                    <div class="wrimagecard-topimage_header" style="background-color:rgba(3, 173, 252, 0.1) ">
-                      <center><i class=" fa fa-solid fa-user-group" style="color:#03adfc"></i></center>
-                    </div>
-                    <div class="wrimagecard-topimage_title">
-                      <h4 class="text-center">Contacts
-                      </h4>
-                        <p class="text-center" style="font-weight: 100; font-size: 15px;">Add & Edit</p>
                     </div>
                 </a>
             </div>
